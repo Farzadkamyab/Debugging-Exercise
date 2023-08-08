21d87a74609da98891c0626c77b4624171ca6289
Farzad kamyab <Farzadkamyabofficial@gmail.com>
2023-08-08 18:50:58 +0330

    fix: add admin config to installed app

--- Todo Project/Config/settings.py
+++ Todo Project/Config/settings.py
@@ -9,11 +9,13 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "Config",
     "Home",
 ]
 
